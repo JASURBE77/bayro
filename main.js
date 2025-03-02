31f423a8fb6e14dd35500039a50aa4fe7aa3f4df
JASURBE77 <animesila89@gmail.com>
2025-03-02 10:20:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function light() {
+    var aisha = document.querySelector('.header-item')
+    var miri = document.querySelector('.flag-text2')
+    var body = document.querySelector('body');
+    var sherka = document.querySelector('header')
+    var qidir = document.querySelector('input')
+    sherka.style.backgroundColor = 'black';
+    body.style.backgroundColor = 'white';
+    body.style.transition = '300ms ease all'
+    sherka.style.color = 'white';
+    body.style.color = 'black';
+    input.style.backgroundColor = 'black';
+
+
+}
+function dark() {
+    var sherka = document.querySelector('header')
+    var body = document.querySelector('body');
+    var input = document.querySelector('input')
+    sherka.style.color = 'black';
+    sherka.style.color = 'black';
+    sherka.style.backgroundColor = 'white';
+    body.style.backgroundColor = 'black';
+    body.style.transition = '300ms ease all'
+    body.style.color = 'white';
+    input.style.backgroundColor = 'white';
+}
